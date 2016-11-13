72d587b43b34935f265553304a66cdffd12d8ca9
Bakht <ebr163@mail.ru>
2016-11-13 20:21:15 +0400

    craate modal window for change preference

--- webserver/src/main/assets/tools/scripts/preferences/preferences.js
+++ webserver/src/main/assets/tools/scripts/preferences/preferences.js
@@ -10,6 +10,9 @@ function addPreference(data){
 
 function loadPreferences(preferences){
     for(var i=0; i<preferences.length; i++) {
-        $("table tbody").append('<tr><td>'+preferences[i].key+'</td><td>'+preferences[i].value+'</td><td>'+preferences[i].type+'</td></tr>');
+        $("table tbody").append('<tr><td id="'+preferences[i].key +'_key"><a class="waves-effect" id="'+preferences[i].key +'" href="#medit" title="Edit"><i class="material-icons">mode_edit</i></a>'+preferences[i].key+'</td><td id="'+preferences[i].key +'_value">'+preferences[i].value+'</td><td id="'+preferences[i].key +'_type">'+preferences[i].type+'</td></tr>');
+        $('#'+preferences[i].key).on('click', function(){
+                    $("#pref_key_edit").val(this.id)
+        })
     }
 }
